cb1a95af7698315372299bc2f35b30da149e464e
Peter990628 <haebyuk35@gmail.com>
2026-07-04 14:01:32 +0900

    기어 성공 코드

--- rokey/gear.py
+++ rokey/gear.py
@@ -3,6 +3,8 @@
 import rclpy
 import DR_init 
 from time import sleep
+# from DR_common2 import *
+# from dsr_msgs2.srv import MoveStop
 
 ROBOT_ID   = "dsr01"
 ROBOT_MODEL= "m0609"
@@ -41,9 +43,8 @@ def main(args=None):
             DR_MV_MOD_REL,
             DR_AXIS_Z,
             DR_SSTOP,
-            drl_script_stop,
-            
-            
+            get_current_posx,
+            release_force,
         )
 
         from DR_common2 import posj, posx
@@ -51,6 +52,32 @@ def main(args=None):
     except ImportError as e:
         node.get_logger().info(f"Error importing DSR_ROBOT2 : {e}")
         return
+
+    # move_stop_client = node.create_client(MoveStop, "motion/move_stop")
+
+    # def stop_motion(stop_mode=DR_SSTOP):
+    #     if not move_stop_client.wait_for_service(timeout_sec=1.0):
+    #         node.get_logger().error("motion/move_stop service is not available")
+    #         return False
+
+    #     req = MoveStop.Request()
+    #     req.stop_mode = stop_mode
+
+    #     future = move_stop_client.call_async(req)
+    #     rclpy.spin_until_future_complete(node, future)
+
+    #     try:
+    #         result = future.result()
+    #     except Exception as e:
+    #         node.get_logger().error(f"motion/move_stop service call failed: {e}")
+    #         return False
+
+    #     if result is None or not result.success:
+    #         node.get_logger().error("motion/move_stop failed")
+    #         return False
+
+    #     node.get_logger().info("motion/move_stop success")
+    #     return True
 	
     set_tool("Tool Weight_1") # 패드에 있는 이름으로 해야 함 
     set_tcp("GripperDA_v1") # 패드에 있는 이름으로 해야 함
@@ -113,8 +140,8 @@ def main(args=None):
     movel(posx(0,0,-80,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
     grip()
     movel(posx(0,0,80,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
-    movel(posx(513.99, -5.83, 127.46, 46.11, 180.00, 36.58), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
-    movel(posx(513.99, -5.83, 47.46, 46.11, 180.00, 36.58), mod=DR_MV_MOD_ABS, ref=DR_BASE ,radius = 70, v = 1500, a = 1500)
+    movel(posx(513.30, -5.58, 138.30, 102.11, -179.58, 101.50), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
+    movel(posx(513.30, -5.58, 58.30, 102.11, -179.58, 101.50), mod=DR_MV_MOD_ABS, ref=DR_BASE ,radius = 70, v = 1500, a = 1500)
     release()
     movel(posx(0,0,60,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
 
@@ -127,8 +154,8 @@ def main(args=None):
     movel(posx(0,0,-80,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
     grip()
     movel(posx(0,0,80,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
-    movel(posx(608.13, -52.07, 125.64, 175.64, -179.87, 166.24), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
-    movel(posx(608.13, -52.07, 45.64, 175.64, -179.87, 166.24), mod=DR_MV_MOD_ABS, ref=DR_BASE , radius = 70, v = 1500, a = 1500)
+    movel(posx(607.99, -51.66, 145.46, 106.35, -178.97, 106.85), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
+    movel(posx(607.99, -51.66, 65.46, 106.35, -178.97, 106.85), mod=DR_MV_MOD_ABS, ref=DR_BASE , radius = 70, v = 1500, a = 1500)
     release()
     movel(posx(0,0,60,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
 
@@ -141,20 +168,43 @@ def main(args=None):
     movel(posx(0,0,-80,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
     grip()
     movel(posx(0,0,80,0,0,0), mod = DR_MV_MOD_REL, radius = 70, v = 1500, a = 1500)
-    movel(posx(576.50, -2.30, 105.52, 54.68, 179.90, 47.50), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
-    movel(posx(576.50, -2.30, 47, 54.68, 179.90, 47.50), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
+    movel(posx(574.65, -0.18, 144.17,20.60,-178.99, 15.24), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
+    movel(posx(574.65, -0.18, 64.17,20.60,-178.99, 15.24), mod=DR_MV_MOD_ABS, ref=DR_BASE  , radius = 70, v = 1500, a = 1500)
+    node.get_logger().info("move done")
+    wait(0.5)
     set_desired_force([0,0,-10,0,0,0],[0,0,1,0,0,0])
+    wait(0.5)
+    node.get_logger().info("set_desired_force done")
+    wait(0.5)
+    amove_periodic(amp =[0,0,0,0,0,8], period=[0,0,0,0,0,1.5], atime=0.5, repeat=5, ref=DR_BASE)
+    wait(0.5)
+    node.get_logger().info("amove_periodic_start")  
+    wait(0.5)
+    # while True:
+    #     # node.get_logger().info("amove_periodic_start")
+    #     # amove_periodic(amp =[0,0,0,0,0,8], period=[0,0,0,0,0,1.5], atime=0.5, repeat=10, ref=DR_BASE)
+    #     node.get_logger().info("amove_periodic_done")
+    #     wait(0.5)
+    #     if check_position_condition(DR_AXIS_Z,43,46,DR_BASE,DR_MV_MOD_ABS):
+    #         # stop_motion(DR_SSTOP)
+    #         wait(0.5)
+    #         node.get_logger().info("check_position_condition_done")
+    #         wait(0.5)
+    #         break
 
-    amove_periodic(amp =[0,0,0,0,0,8], period=[0,0,0,0,0,1.5], atime=0.5, repeat=10, ref=DR_BASE)
+    wait(0.5)
+    set_desired_force([0,0,-20,0,0,0],[0,0,1,0,0,0])
     while True:
-        if check_position_condition(DR_AXIS_Z,43,46,DR_BASE,DR_MV_MOD_ABS):
-            stop(DR_SSTOP)
-            break
-
-    set_desired_force([0,0,-10,0,0,0],[0,0,1,0,0,0])
-    while True:
-        if check_position_condition(DR_AXIS_Z,0,38,DR_BASE,DR_MV_MOD_ABS):
+        if check_position_condition(DR_AXIS_Z,0,38,DR_BASE,DR_MV_MOD_ABS) == 0 :
+            current_posx, _ = get_current_posx(DR_BASE)
+            node.get_logger().info(f"current z = {current_posx[2]}")
+            
             release()
+            wait(0.5)
+            release_force()
+            wait(0.5)
             movel(posx(0,0,60,0,0,0), mod = DR_MV_MOD_REL, v = 1500, a = 1500)
+            wait(0.5)
+            movej(posj(0, 0, 90, 0, 90, 0), vel=VELOCITY, acc=ACC)
             break
     # -------4 끝 --------
