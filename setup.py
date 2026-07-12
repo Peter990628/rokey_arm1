72709d65cc34edb97170956c5cd9f70c835bc31f
Peter990628 <haebyuk35@gmail.com>
2026-07-12 15:06:49 +0900

    total_test 용 브렌치 이므로 쓸모 없는거 지움, setup.py 도 수정함

--- setup.py
+++ setup.py
@@ -7,15 +7,20 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml'],
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='PETER',
     maintainer_email='haebyuk35@gmail.com',
-    description='move_node',
+    description='Pharmacy assistant robot control nodes',
     license='Apache-2.0',
     extras_require={
         'test': [
@@ -24,15 +29,14 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'move = rokey.move:main',
-            'grip_test = rokey.grip_test:main',
-            'gear = rokey.gear:main',
             'go_home = rokey.drug_peter.go_home:main',
-            'paper_bag_test = rokey.drug_peter.paper_bag_test:main',
-            'task_manager_bridge = rokey.drug_peter.task_manager_bridge:main',
+            'task_manager_bridge = rokey.task_manager_bridge:main',
             'manipulator_test_2 = rokey.manipulator_test_2:main',
             'pour_pills_rotate = rokey.pour_pills_rotate:main',
-            'test1 = rokey.test1:main',
+            'test_spin_lid = rokey.test_spin_lid:main',
+            'test_storage_grasp = rokey.test_storage_grasp:main',
+            'test_tcp = rokey.test_tcp:main',
+            'go_home = rokey.go_home:main',
         ],
     },
 )
